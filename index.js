b49dbb344debac11f46a8a7a9d702a4d52e105ad
Kerny <kerny14@gmail.com>
2023-02-09 22:02:07 -0500

    commit tree e311c0bfc4a828c5a71684b51d3ede08f2945a9b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+saturdayFun();
+
+function saturdayFun() {
+    return 'This Saturday, I want to roller-skate!'
+       
+}
+
+function mondayWork(){
+    return 'This Monday, I will go to the office.'
+}
+
+function wrapAdjective(){
+
+}
